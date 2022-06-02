41b1c5acb284986383860c25f809018926488a86
konok101 <rockykonok@gmail.com>
2022-06-03 01:24:12 +0600

    display ui and sign in and sign out proces

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
  
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { useState } from 'react';
 
 
 const auth = getAuth(app);
 function App() {
+  const [user, setUser]= useState({});
  const provider = new GoogleAuthProvider();
+
  const handleGoogleSignIn= ()=>{
    signInWithPopup(auth, provider)
    .then(result=>{
      const user = result.user;
-     console.log(user)
+     console.log(user);
+     setUser(user)
    })
    .catch(error =>{
      console.error('error', error)
    })
+ }
 
+ const handleSingnOut= ()=>{
+   signOut(auth)
+   .then( ()=> {
+     setUser({});
+   })
+   .catch(error =>{
+     setUser({});
+   })
 
  }
 
  
   return (
     <div className="App">
- <button onClick={handleGoogleSignIn}>Google sign in</button>
+ {
+   user.email ? <button onClick={handleSingnOut}>SignOut</button>  :
+  <button onClick={handleGoogleSignIn}>Google sign in</button>
+   
+ }
+ <h2>Name: {user.displayName}</h2>
+ <h6>Email: {user.email}</h6>
+ <img src={user.photoURL} alt="" />
     </div>
   );
 }
